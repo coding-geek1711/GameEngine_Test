e633ce08d78fc0c6222c86472e95612a66063488
bhatji3383 <memotoskbhat@gmail.com>
2021-05-06 20:31:27 +0530

    weird collision detection bug

--- main.cpp
+++ main.cpp
@@ -17,17 +17,30 @@ Maheswaran and S krishna Bhat\n";
         // std::cout << "tihs is nice\n";
         if (strcmp(argv[1], "sfml") == 0)
         {
-            sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+            sf::ContextSettings settings;
+            settings.antialiasingLevel = 8;
+            sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!", sf::Style::Default, settings);
             sf::Clock clk;
             sf::RectangleShape rectshape({50.f, 50.f});
             rectshape.setPosition({100.f, 100.f});
             rectshape.setFillColor(sf::Color::Green);
             rectshape.setOrigin(25.f, 25.f);
+            sf::FloatRect flrect;
+
+            // std::cout << "" << '\n';
 
             sf::CircleShape circshape(50.f);
             circshape.setFillColor(sf::Color::Magenta);
             circshape.setOrigin({25.f, 25.f});
             circshape.setPosition({150.f, 150.f});
+            sf::FloatRect flcirc;
+
+            sf::Text te;
+            sf::Font f;
+            f.loadFromFile("fonts/FiraCode-Regular.ttf");
+            te.setFont(f);
+            te.setCharacterSize(24);
+            te.setFillColor(sf::Color::White);
 
             sf::Vector2f vel(10.f, 0.f);
             float spd = 10.f;
@@ -39,18 +52,35 @@ Maheswaran and S krishna Bhat\n";
                     if (event.type == sf::Event::Closed)
                         window.close();
                 }
+                flrect = rectshape.getGlobalBounds();
+                std::cout << flrect.left << '\n';
+                flcirc = circshape.getGlobalBounds();
+
+                flcirc.intersects(flrect) ? te.setString("True") : te.setString("False");
+
                 rectshape.rotate(spd * clk.getElapsedTime().asSeconds());
                 rectshape.move(vel * clk.getElapsedTime().asSeconds());
                 circshape.move(sf::Vector2f({0.f, -10.f}) * clk.getElapsedTime().asSeconds());
-                clk.restart();
+                // std::cout << flrect.intersects(flcirc);
                 window.clear();
+
                 window.draw(rectshape);
                 window.draw(circshape);
+
+                clk.restart();
+                window.clear();
+
+                window.draw(rectshape);
+                window.draw(circshape);
+                // std::cout << flrect.intersects(flcirc);
+                window.draw(te);
                 window.display();
             }
         }
         // std::cout << argv[1];
     }
+
+    // decide the number of enemies and stuff
 
     return 0;
 }
