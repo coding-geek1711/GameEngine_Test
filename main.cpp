49224bf45206ace1f224c52c9ff9b3e55c0aa235
bhatji3383 <memotoskbhat@gmail.com>
2021-05-08 15:02:53 +0530

    Merge branch 'master' of github.com:coding-geek1711/GameEngine_Test

--- main.cpp
+++ main.cpp
@@ -22,24 +22,18 @@
     10. rotate rotates the object
 */
 
-sf::Vector2f getVel(float x, float y)
+void getM_Vertices(sf::VertexArray *m_vertices, int frame_width, int *frame_height)
 {
-    return sf::Vector2f({x, y});
-}
-
-void getM_Vertices(sf::VertexArray* m_vertices, int* frame_width, int* frame_height)
-{
-    for(int i = 0; i < *frame_width; i++)
+    for (int i = 0; i < frame_width; i++)
     {
-        for(int j = 0; j < *frame_height; j++)
+        for (int j = 0; j < *frame_height; j++)
         {
-            sf::Vertex* quad = &(*m_vertices)[(i + j * (*frame_width)) * 4];
+            sf::Vertex *quad = &(*m_vertices)[(i + j * (frame_width)) * 4];
 
             quad[0].position = sf::Vector2f(i * 100, j * 100);
             quad[1].position = sf::Vector2f((i + 1) * 100, j * 100);
             quad[2].position = sf::Vector2f((i + 1) * 100, (j + 1) * 100);
             quad[3].position = sf::Vector2f(i * 100, (j + 1) * 100);
-
 
             quad[0].texCoords = sf::Vector2f(0.f, 0.f);
             quad[1].texCoords = sf::Vector2f(25.f, 0.f);
@@ -49,8 +43,33 @@ void getM_Vertices(sf::VertexArray* m_vertices, int* frame_width, int* frame_hei
     }
 }
 
+sf::Vector2f getVel(float x, float y)
+{
+    return sf::Vector2f({x, y});
+}
 
-void monitorPoll(sf::RenderWindow* window, sf::Vector2f* vel)
+void getM_Vertices(sf::VertexArray *m_vertices, int frame_width, int frame_height)
+{
+    for (int i = 0; i < frame_width; i++)
+    {
+        for (int j = 0; j < frame_height; j++)
+        {
+            sf::Vertex *quad = &(*m_vertices)[(i + j * (frame_width)) * 4];
+
+            quad[0].position = sf::Vector2f(i * 100, j * 100);
+            quad[1].position = sf::Vector2f((i + 1) * 100, j * 100);
+            quad[2].position = sf::Vector2f((i + 1) * 100, (j + 1) * 100);
+            quad[3].position = sf::Vector2f(i * 100, (j + 1) * 100);
+
+            quad[0].texCoords = sf::Vector2f(0.f, 0.f);
+            quad[1].texCoords = sf::Vector2f(25.f, 0.f);
+            quad[2].texCoords = sf::Vector2f(25.f, 25.f);
+            quad[3].texCoords = sf::Vector2f(0.f, 25.f);
+        }
+    }
+}
+
+void monitorPoll(sf::RenderWindow *window, sf::Vector2f *vel)
 {
     sf::Event event;
     while (window->pollEvent(event))
@@ -87,9 +106,8 @@ void monitorPoll(sf::RenderWindow* window, sf::Vector2f* vel)
     }
 }
 
-
 template <class T>
-T renderObject(const sf::Vector2f& objectShape, const sf::Vector2f& objectPosition, sf::Color color, const sf::Vector2f& objectOrigin)
+T renderObject(const sf::Vector2f &objectShape, const sf::Vector2f &objectPosition, sf::Color color, const sf::Vector2f &objectOrigin)
 {
     T object(objectShape);
     object.setPosition(objectPosition);
@@ -99,7 +117,7 @@ T renderObject(const sf::Vector2f& objectShape, const sf::Vector2f& objectPositi
 }
 
 template <class T>
-T renderObject(const float objectShape, const sf::Vector2f& objectPosition, sf::Color color, const sf::Vector2f& objectOrigin)
+T renderObject(const float objectShape, const sf::Vector2f &objectPosition, sf::Color color, const sf::Vector2f &objectOrigin)
 {
     T object(objectShape);
     object.setPosition(objectPosition);
@@ -107,74 +125,140 @@ T renderObject(const float objectShape, const sf::Vector2f& objectPosition, sf::
     object.setOrigin(objectOrigin);
     return object;
 }
-
 
 int main(int argc, char **argv)
 {
-    std::cout << "hi this is the first iteration of Game Engine by\
-    Maheswaran and S krishna Bhat\n";
-    
-    int frame_width = 800;
-    int frame_height = 800;
-
-    sf::VertexArray m_vertices;
-    sf::Texture texture;
-
-    m_vertices.setPrimitiveType(sf::Quads);
-    m_vertices.resize(frame_height * frame_width * 4);
-
-    getM_Vertices(&m_vertices, &frame_width, &frame_height);
-    if(!texture.loadFromFile("../tiles.png"))
-        std::cout<<"Error in loading textures" << std::endl;
-
-    sf::ContextSettings settings;
-    settings.antialiasingLevel = 8;
-
-    sf::RenderWindow window(sf::VideoMode(frame_width, frame_height), "collision_detection.maybe", sf::Style::Default, settings);
-    sf::Clock clk;
-
-    sf::RectangleShape rectshape = renderObject<sf::RectangleShape>({50.f, 50.f}, {400.f, 150.f}, sf::Color::Yellow, {25.f, 25.f});
-    sf::CircleShape circshape = renderObject<sf::CircleShape>(50.f, {550.f, 550.f}, sf::Color::Magenta, {25.f, 25.f});
-
-    sf::FloatRect flcirc;
-    sf::FloatRect flrect;
-
-    sf::Text te;
-    sf::Font f;
-    f.loadFromFile("fonts/FiraCode-Regular.ttf");
-    te.setFont(f);
-    te.setCharacterSize(24);
-    te.setFillColor(sf::Color::White);
-
-    sf::Vector2f vel(0.f, 0.f);
-    float spd = 10.f;
-    while (window.isOpen())
+    if (argc > 1)
     {
-        
-        monitorPoll(&window, &vel);
-        window.clear();
-        flrect = rectshape.getGlobalBounds();
-        flcirc = circshape.getGlobalBounds();
-        window.draw(m_vertices, &texture);
-        if (!flrect.intersects(flcirc))
+        if (!strcmp(argv[1], "colltrial"))
         {
-            flcirc.intersects(flrect) ? te.setString("True") : te.setString("False");
+            std::cout << "hi this is the first iteration of Game Engine by\
+    Maheswaran and S krishna Bhat\n";
 
-            circshape.move(vel * clk.getElapsedTime().asSeconds());
+            int frame_width = 800;
+            int frame_height = 800;
 
-            rectshape.rotate(spd * clk.getElapsedTime().asSeconds());
+            sf::VertexArray m_vertices;
+            sf::Texture texture;
 
-            window.draw(rectshape);
-            window.draw(circshape);
+            m_vertices.setPrimitiveType(sf::Quads);
+            m_vertices.resize(frame_height * frame_width * 4);
+
+            getM_Vertices(&m_vertices, frame_width, frame_height);
+            if (!texture.loadFromFile("assets/textures/tiles.png"))
+                std::cout << "Error in loading textures" << std::endl;
+
+            sf::ContextSettings settings;
+            settings.antialiasingLevel = 8;
+
+            sf::RenderWindow window(sf::VideoMode(frame_width, frame_height), "collision_detection.maybe", sf::Style::Default, settings);
+            sf::Clock clk;
+
+            sf::RectangleShape rectshape = renderObject<sf::RectangleShape>({50.f, 50.f}, {400.f, 150.f}, sf::Color::Yellow, {25.f, 25.f});
+            sf::CircleShape circshape = renderObject<sf::CircleShape>(50.f, {550.f, 550.f}, sf::Color::Magenta, {25.f, 25.f});
+
+            sf::FloatRect flcirc;
+            sf::FloatRect flrect;
+
+            sf::Text te;
+            sf::Font f;
+            f.loadFromFile("fonts/FiraCode-Regular.ttf");
+            te.setFont(f);
+            te.setCharacterSize(24);
+            te.setFillColor(sf::Color::White);
+
+            sf::Vector2f vel(0.f, 0.f);
+            float spd = 10.f;
+            while (window.isOpen())
+            {
+
+                monitorPoll(&window, &vel);
+                window.clear();
+                flrect = rectshape.getGlobalBounds();
+                flcirc = circshape.getGlobalBounds();
+                window.draw(m_vertices, &texture);
+                if (!flrect.intersects(flcirc))
+                {
+                    flcirc.intersects(flrect) ? te.setString("True") : te.setString("False");
+
+                    circshape.move(vel * clk.getElapsedTime().asSeconds());
+
+                    rectshape.rotate(spd * clk.getElapsedTime().asSeconds());
+
+                    window.draw(rectshape);
+                    window.draw(circshape);
+                }
+
+                clk.restart();
+
+                window.draw(te);
+                window.display();
+            }
         }
 
-        clk.restart();
+        if (!strcmp(argv[1], "texturedbg"))
+        {
+            int frame_height = 640;
+            int frame_width = 640;
+            sf::RenderWindow window(sf::VideoMode(frame_height, frame_width), "TileMap");
 
-        window.draw(te);
-        window.display();
-    
+            // // multiple quads
+            sf::VertexArray m_vertices;
+            sf::Texture texture;
+
+            m_vertices.setPrimitiveType(sf::Quads);
+            m_vertices.resize(frame_height * frame_width * 4);
+
+            const int tiles[] = {
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+            };
+            getM_Vertices(&m_vertices, frame_width, frame_height);
+            // for(int i = 0; i < frame_width; i ++)
+            // {
+            //     for(int j = 0; j < frame_height; j++)
+            //     {
+            //         sf::Vertex* quad = &m_vertices[(i + j * frame_width) * 4];
+
+            //         // int tileNumber = tiles[i];
+            //         quad[0].position = sf::Vector2f(i * 100, j * 100);
+            //         quad[1].position = sf::Vector2f((i + 1) * 100, j * 100);
+            //         quad[2].position = sf::Vector2f((i + 1) * 100, (j + 1) * 100);
+            //         quad[3].position = sf::Vector2f(i * 100, (j + 1) * 100);
+
+            //         quad[0].texCoords = sf::Vector2f(0.f, 0.f);
+            //         quad[1].texCoords = sf::Vector2f(25.f, 0.f);
+            //         quad[2].texCoords = sf::Vector2f(25.f, 25.f);
+            //         quad[3].texCoords = sf::Vector2f(0.f, 25.f);
+            //     }
+            // }
+
+            if (!texture.loadFromFile("assets/textures/tiles.png"))
+            {
+                std::cout << "Error loading textures" << std::endl;
+            }
+
+            while (window.isOpen())
+            {
+                sf::Event event;
+                while (window.pollEvent(event))
+                {
+                    if (event.type == sf::Event::Closed)
+                    {
+                        window.close();
+                    }
+                }
+
+                window.clear();
+                window.draw(m_vertices, &texture);
+                window.display();
+            }
+        }
     }
-    
+
     return 0;
 }
-
